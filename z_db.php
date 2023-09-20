1b7a4a53348e5bf5c4b9273b8c7ddc859c3a0dbf
Sia Chachra <siachachra@gmail.com>
2023-09-19 21:21:10 -0400

    init

---
+++ z_db.php
@@ -0,0 +1,6 @@
+<?php
+$con = new mysqli("172.31.22.43", "Vinnie200547583", "rieqMag-x1", "Vinnie200547583");
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+?>
